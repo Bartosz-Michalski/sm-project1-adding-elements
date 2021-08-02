f21c30bfd95c02f117f2d8c976268417b6ec1158
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-02 17:18:40 +0200

    Task4JS - ability to add squares to the page

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
 const mainButton = document.querySelector(".main-button");
 const squaresContainer = document.querySelector(".squares-container");
 
-mainButton.addEventListener("click", buttonClick);
-
 const buttonClick = function () {
-  const square = createElement("div");
+  const square = document.createElement("div");
+  square.classList.add("square");
+  squaresContainer.appendChild(square);
 };
+
+mainButton.addEventListener("click", buttonClick);
