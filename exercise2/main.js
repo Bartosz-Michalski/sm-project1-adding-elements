cc65bb8c7069ba1c39c6bb3f486ac8dfdcdd99ef
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-02 20:38:00 +0200

    Ex2Task5JS - bigger font for any number divisible by 3

--- exercise2/main.js
+++ exercise2/main.js
@@ -7,6 +7,11 @@ btn.addEventListener("click", () => {
   const listItem = document.createElement("li");
 
   listItem.textContent = number;
-  number += 2;
   list.appendChild(listItem);
+
+  if (number % 3 == 0) {
+    listItem.classList.toggle("big");
+  }
+
+  number += 2;
 });
