d3e31e28982736a54af2db630be45ff191fc7640
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-02 16:03:14 +0200

    Task3JS - created a square element (div) with a class

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const mainButton = document.querySelector(".main-button");
+const squaresContainer = document.querySelector(".squares-container");
 
 mainButton.addEventListener("click", buttonClick);
 
-const buttonClick = function () {};
+const buttonClick = function () {
+  const square = createElement("div");
+};
