d922734e739647a545207a474c82d2e1d3dec557
Bartosz Michalski <bartosz.j.michalski@gmail.com>
2021-08-02 18:05:39 +0200

    Task5JS - added counter to squares

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
 const mainButton = document.querySelector(".main-button");
 const squaresContainer = document.querySelector(".squares-container");
 
+// const counter = function (number) {
+//   return number++;
+// };
+let number = 1;
+
 const buttonClick = function () {
   const square = document.createElement("div");
+
   square.classList.add("square");
+  square.textContent = number;
   squaresContainer.appendChild(square);
+  number += 1;
 };
 
 mainButton.addEventListener("click", buttonClick);
